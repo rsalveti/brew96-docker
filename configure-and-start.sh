4ec245e90feb165cddd7d65a8f34e702809fc6c6
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-03-22 01:33:26 -0300

    Initial changes for brew96
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- configure-and-start.sh
+++ configure-and-start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-AVR=${AVR:-brewpi.arduino.local:23}
-sed -i.bak "s#^port.*#port = socket://$AVR#" /home/brewpi/settings/config.cfg
-sed -i.bak "s#^altport.*#altport = socket://$AVR#" /home/brewpi/settings/config.cfg
-
 TZ=${TZ:-UTC}
 cp "/usr/share/zoneinfo/$TZ" /etc/localtime
 
